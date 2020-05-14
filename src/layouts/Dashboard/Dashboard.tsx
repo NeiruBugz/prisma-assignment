1f49445bc24f3b71dca65de874711877bdd052a9
Nail Badiullin <badiullin.nail@gmail.com>
2020-05-14 14:34:28 +0400

    [layout] add csv to json converter add conversion computation function

--- src/layouts/Dashboard/Dashboard.tsx
+++ src/layouts/Dashboard/Dashboard.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { csvToJSONConverter } from '../../utils';
 
 export const Dashboard = () => {
+  const [rows, setRows] = useState();
+
+  useEffect(() => {
+    fetch('https://prisma-assignment-api.herokuapp.com/api/csv-data')
+      .then((response) => response.text())
+      .then((result) => {
+        const parsedCsv = csvToJSONConverter(result);
+        debugger;
+        setRows(parsedCsv);
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }, []);
   return <section className="dashboard">Dashboard</section>;
 };
---
+++ src/utils/converters.ts
@@ -0,0 +1,21 @@
+export const csvToJSONConverter = (csv: string) => {
+  const lines = csv.split('\n');
+  const result: any = [];
+  let headers = lines[0].split(',');
+  headers = headers.map((header) => header.match(/\w+/)).flat();
+
+  for (let i = 1; i < lines.length; i++) {
+    const obj: any = {};
+    const currentLine = lines[i].split(',');
+    for (let j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentLine[j]?.replace(/['"]+/g, '');
+    }
+
+    result.push(computeConversion(obj));
+  }
+
+  return result;
+};
+
+const computeConversion = (obj: any) =>
+  Object.assign(obj, { conversion: (Number(obj.trials) / Number(obj.installs)).toFixed(1) });
